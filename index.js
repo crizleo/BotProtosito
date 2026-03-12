1cf8d25ed9bfd6ffc0889749a2574f02095d4716
Crizleo <christianleonardo2002@gmail.com>
2026-03-12 16:57:06 -0500

    Adicción de manejo de URL fraudulentas v1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Client, GatewayIntentBits, Partials, Events } = require("discord.js");
+const axios = require("axios");
 require("dotenv").config();
 const cron = require("node-cron");
 
 const client = new Client({
   intents: [
+    GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
@@ -35,26 +37,76 @@ client.once(Events.ClientReady, () => {
   );
 });
 
-client.on(Events.MessageCreate, (message) => {
-  if (message.author.bot) return;
+client.on(Events.MessageCreate, (mensaje) => {
+  validarMensaje(mensaje, "C");
+});
 
-  console.log("asdasd");
+client.on(Events.MessageUpdate, (mensaje) => {
+  validarMensaje(mensaje, "U");
+});
 
-  const author = message.author.id;
-  const content = message.content;
+async function validarMensaje(mensaje, act) {
+  console.log(mensaje);
 
-  const regexUrl = /\bhttps?:\/\/\S+/gi;
+  if (mensaje.author?.bot || mensaje.canalId == process.env.SILENT_CANAL)
+    return;
 
-  const enlacesEncontrados = message.content.match(regexUrl);
+  let author;
+  let content;
+
+  if (act == "C") {
+    author = mensaje.author?.id;
+    content = mensaje.content;
+  } else if (act == "U") {
+    author = mensaje.reactions.message.author.id;
+    content = mensaje.reactions.message.content;
+  }
+
+  const regexUrl = /https?:\/\/[^\s\)]+/gi;
+  const enlacesEncontrados = content.match(regexUrl);
+
+  let resultados;
+  let urlLimpia;
 
   if (enlacesEncontrados) {
-    enlacesEncontrados.forEach((url) => {
-      const urlBase64 = Buffer.from(url).toString("Base64").replace(/=/g, "");
+    for (const url of enlacesEncontrados) {
+      urlLimpia = url.replace(/\)$/, "");
 
-      console.log(url);
-      console.log(urlBase64);
-    });
+      resultados = await validarUrl(urlLimpia);
+
+      if (resultados.esPeligrosa) {
+        await mensaje.delete();
+        break;
+      }
+    }
   }
-});
+}
+
+async function validarUrl(url) {
+  const urlBase64 = Buffer.from(url).toString("Base64").replace(/=/g, "");
+
+  try {
+    const response = await axios.get(process.env.URL_VALIDATOR + urlBase64, {
+      headers: { "x-apikey": process.env.VIRUSTOTAL_API_KEY },
+    });
+
+    const status = response.data.data.attributes.last_analysis_stats;
+
+    const reportes = status.malicious || 0 + status.phishing || 0;
+
+    return {
+      esPeligrosa: reportes > 0,
+      cantidadAlertas: reportes,
+      detalles: status,
+    };
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      console.log("URL nueva, no hay análisis previo en VirusTotal.");
+    } else {
+      console.error("Error en la API de VirusTotal:", error.message);
+    }
+    return { esPeligrosa: false };
+  }
+}
 
 client.login(process.env.TOKEN);
